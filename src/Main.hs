aaf7157298f2d46890d42fbb06665708d44bed48
Christian Bunk <christianb.github@googlemail.com>
2011-07-01 16:01:14 +0200

    implement IO functions for -i, -p and -c

--- src/Main.hs
+++ src/Main.hs
@@ -4,39 +4,26 @@ import System.Directory
 import System.IO   
   
 import Index  
-  
-replaceEqualWithSpace :: String -> String
-replaceEqualWithSpace [] = []
-replaceEqualWithSpace (w:ws) = 
-    if w /= '='
-        then w:(replaceEqualWithSpace ws)
-        else replaceEqualWithSpace ws
 
-removeEqual :: (String,String) -> (String,String)
-removeEqual pair = 
-    let h = fst pair
-        s = snd pair
-    in (h,replaceEqualWithSpace s)
-        
-makeTupel :: String -> (String, String)
-makeTupel w = span ('='/=) w
-
-p :: IO ()
-p = print "call method for -p"
-
-t :: IO ()
-t = print "call method for -t"
-
-dispatch :: [(String, IO ())]  
-dispatch =  [ ("-p", p), ("-t", t)]
-
-getMyArgs :: [(String,String)] -> [String]
-getMyArgs [] = []
-getMyArgs (p:ps) = (fst p):(getMyArgs ps)
-
+type Text = String
+type Wort = String
+type File = String
+ 
 -- get a list of text and a list of files and make a list of pairs
-makePair :: [String] -> [String] -> [(String,String)]
+makePair :: [Text] -> [File] -> [(Text,File)]
 makePair text files = zip text files
+
+-- function for argument -c
+printWordNumber :: [(Wort, [(File, [Int])])] -> IO ()
+printWordNumber index = print (length (map fst index))
+
+-- function for argument -p
+printIndex :: [(Wort, [(File, [Int])])] -> IO ()
+printIndex index = print' index
+
+-- function for argument -i
+createIndex :: [(Text,File)] -> [(Wort, [(File, [Int])])]
+createIndex content = index content
 
 main = do
     -- list with files
@@ -46,23 +33,8 @@ main = do
     content_list <- mapM readFile files
     
     let content = makePair content_list files 
-    let idx = index content
+    let idx = createIndex content
     
-    print' idx
+    printIndex idx
     
-
-{-main = do  
-   args <- getArgs  
-   progName <- getProgName  
-   --putStrLn "The arguments are:"  
-   --mapM putStrLn args  
-   --putStrLn "The program name is:"  
-   --putStrLn progName
-   let tupels = map makeTupel args -- list with tupels
-   let tupels_cleaned = map removeEqual tupels -- list with tupels without equall sign
-   mapM print tupels_cleaned
-   let only_args = getMyArgs tupels_cleaned
-   map only_args
-   --let (command:args) = tupels_cleaned
-   let (Just action) = lookup "-p" dispatch  
-   action-}
+    printWordNumber idx
